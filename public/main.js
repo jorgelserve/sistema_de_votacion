e703bbe5410746f6bb624dc3777b79c54c132794
Jorge Luis Serna <jorgelserve@icloud.com>
2018-03-08 23:54:48 -0500

    version 1.0.0

--- public/main.js
+++ public/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var socket = io.connect('http://localhost:8080', { 'forceNew': true });
 moment.locale('es');
 
@@ -7,46 +9,150 @@ socket.on('messages', function (data) {
 });
 
 socket.on('board-ready', function (data) {
-	empty(document.getElementById('container'))
-	swal(
-		'Tarjeta conectada!',
-		`ARDUINO ${data.board} conectada al puerto ${data.port}`,
-		'success'
-	)
-	document.getElementById('container').innerHTML = `<div style="text-align: center;">
-		<button class="button" id="iniciar">Iniciar</button>
-		<button class="button" id="continuar">Continuar</button>
-	</div>`
-})
+	empty(document.getElementById('container'));
+	swal('Tarjeta conectada!', 'ARDUINO ' + data.board + ' conectada al puerto ' + data.port, 'success');
+	document.getElementById('container').innerHTML = '<div style="text-align: center;">\n\t\t<button class="button" id="iniciar">Iniciar</button>\n\t\t<button class="button" id="continuar">Continuar</button>\n\t</div>';
 
-document.getElementById('iniciar').addEventListener('click', function () {
-	socket.emit('start', null)
-})
+	document.getElementById('iniciar').addEventListener('click', function () {
+		swal({
+			title: 'Introduzca la contraseña',
+			input: 'password',
+			showCancelButton: true,
+			confirmButtonText: 'Submit',
+			showLoaderOnConfirm: true,
+			preConfirm: function preConfirm(password) {
+				return new Promise(function (resolve, reject) {
+					socket.emit('check-password', password);
+					socket.on('system-init', function (data) {
+						if (data.pass) {
+							resolve();
+						} else {
+							reject();
+						}
+					});
+				});
+			},
+			allowOutsideClick: function allowOutsideClick() {
+				return !swal.isLoading();
+			}
+		}).then(function (result) {
+			if (!result.dismiss) {
+				swal({
+					position: 'top-end',
+					type: 'success',
+					title: 'Sistema iniciado exitosamente',
+					showConfirmButton: false,
+					timer: 1500
+				});
+				empty(document.getElementById('container'));
+			}
+		}).catch(function (result) {
+			swal({
+				type: 'error',
+				title: 'Oops...',
+				text: 'Contraseña invalida'
+			});
+		});
+	});
+
+	document.getElementById('continuar').addEventListener('click', function () {
+		socket.emit('keep-going', null);
+		socket.on('keep-going', function (data) {
+			empty(document.getElementById('container')).innerHTML = '<h1 style="text-align: center;">' + data + '</h1>';
+		});
+	});
+});
+
+document.getElementById('resultados').addEventListener('click', function () {
+	swal({
+		title: 'Estas seguro ?',
+		text: "Hacer esto terminará las elecciones y mostrara el resultado, no se podrá votar mas",
+		type: 'warning',
+		showCancelButton: true,
+		confirmButtonColor: '#3085d6',
+		cancelButtonColor: '#d33',
+		confirmButtonText: 'Sí, Mostrar resusltados !'
+	}).then(function (result) {
+		if (result.value) {
+			swal({
+				title: 'Introduzca la contraseña',
+				input: 'password',
+				showCancelButton: true,
+				confirmButtonText: 'Submit',
+				showLoaderOnConfirm: true,
+				preConfirm: function preConfirm(password) {
+					return new Promise(function (resolve, reject) {
+						socket.emit('checkpassword', password);
+						socket.on('checkpassword', function (data) {
+							if (data) {
+								resolve();
+							} else {
+								reject();
+							}
+						});
+					});
+				},
+				allowOutsideClick: function allowOutsideClick() {
+					return !swal.isLoading();
+				}
+			}).then(function (result) {
+				if (result.value) {
+					socket.emit('end', null);
+				}
+			}).catch(function () {
+				swal({
+					type: 'error',
+					title: 'Oops...',
+					text: 'Contraseña invalida'
+				});
+			});
+		}
+	});
+});
+
+socket.on('end', function (data) {
+	empty(document.getElementById('container')).innerHTML = '<div id="chartContainer" style="height: 300px; width: 100%;">';
+	empty(document.getElementById('footer'));
+	var chart = new CanvasJS.Chart("chartContainer", {
+		title: {
+			text: "Elecciones Alcaravanes 2018"
+		},
+		data: [
+		{
+			type: "column",
+			dataPoints: [{ label: 'Blanco', y: data.blanco.votos }, { label: 'Candidato 1', y: data.candidato1.votos }, { label: 'Candidato 2', y: data.candidato2.votos }, { label: 'Candidato 3', y: data.candidato3.votos }]
+		}]
+	});
+	chart.render();
+	socket.on('disconnect', function () {
+		socket.disconnect();
+	});
+});
 
 socket.on('locked', function (data) {
-	data ? document.body.style.backgroundColor = 'red' : document.body.style.backgroundColor = 'initial'
-})
+	data ? document.body.style.backgroundColor = 'red' : document.body.style.backgroundColor = 'initial';
+});
 
 socket.on('voted', function (data) {
-	document.getElementById('container').innerHTML = `<h1 style="text-align: center;">${data}</h1>`
-	console.log(data)
-})
+	document.getElementById('container').innerHTML = '<h1 style="text-align: center;">' + data + '</h1>';
+});
 
 socket.on('down', function (data) {
-	console.log(data)
-})
+	console.log(data);
+});
 
 function empty(element) {
 	if (!(element instanceof HTMLElement)) {
-		throw new TypeError('Expected an element')
+		throw new TypeError('Expected an element');
 	}
 
-	var node
-	while ((node = element.lastChild)) element.removeChild(node)
-	return element
+	var node;
+	while (node = element.lastChild) {
+		element.removeChild(node);
+	}return element;
 }
 
 function startTime() {
-	document.getElementById('header').innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a')
+	document.getElementById('header').innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
 	var t = setTimeout(startTime, 500);
 }
--- server/main.js
+++ server/main.js
@@ -4,10 +4,9 @@ const server = require('http').Server(app)
 const five = require('johnny-five')
 const board = new five.Board()
 const io = require('socket.io')(server)
+const fs = require('fs')
 
 var flag = true
-var votos = 0
-
 
 app.use(express.static('public'))
 
@@ -15,45 +14,100 @@ app.get('/', function (req, res) {
 	res.status(200).send('Hola mun')
 })
 
+
+
 board.on('ready', function() {
-	var blanco = new five.Button(2)
+	var buttons = [
+		new five.Button(2),
+		new five.Button(3),
+		new five.Button(4),
+		new five.Button(5)
+	]
 	var led = new five.Led(13);
 	io.on('connection', function (socket) {
 		setTimeout(() => socket.emit('board-ready', {board: board.type , port: board.port}), 300)
-		socket.on('start', function () {
+		socket.on('check-password', function (data) {
+			if(data == process.env.PASS) {
+				let elecciones = {
+					blanco: {
+						votos: 0
+					},
+					candidato1: {
+						votos: 0
+					},
+					candidato2: {
+						votos: 0
+					},
+					candidato3: {
+						votos: 0
+					}
+				}
+				fs.writeFileSync('server/elecciones.json', JSON.stringify(elecciones, null, 4))
+				// var db = JSON.parse(data)
+			socket.emit('system-init', {
+				pass: true,
+				data: JSON.parse(fs.readFileSync('server/elecciones.json'))
+			})
+		}
 			
 		})
-		blanco.on('press', function() {
-			if(flag) {
-				io.sockets.emit('locked', true)
-				flag = !flag
-				votos += 1
-				io.sockets.emit('voted', votos)
-				setTimeout(() => {
-					flag = true
-					io.sockets.emit('locked', false)
-				}, 2000)
+		socket.on('keep-going', function () {
+			let datos = JSON.parse(fs.readFileSync('server/elecciones.json'))
+			datos = datos.blanco.votos + datos.candidato1.votos + datos.candidato2.votos + datos.candidato3.votos
+			socket.emit('keep-going', datos)
+		})
+		socket.on('start', function (data) {
+			socket.emit('start', 'sistema iniciado')
+		})
+
+		buttons.forEach(function (button, index, array) {
+			button.on('press', function () {
+				if (flag) {
+					let datos = JSON.parse(fs.readFileSync('server/elecciones.json'))
+					switch(index) {
+						case 0:
+							datos.blanco.votos++
+							break
+
+						case 1:
+							datos.candidato1.votos++
+							break
+
+						case 2:
+							datos.candidato2.votos++
+							break
+
+						case 3:
+							datos.candidato3.votos++
+					}
+					fs.writeFileSync('server/elecciones.json', JSON.stringify(datos, null, 4))
+					io.sockets.emit('locked', true)
+					flag = !flag
+					io.sockets.emit('voted', datos.blanco.votos + datos.candidato1.votos + datos.candidato2.votos + datos.candidato3.votos)
+					setTimeout(() => {
+						flag = true
+						io.sockets.emit('locked', false)
+					}, 2000)
+				}
+			})
+		})
+
+		socket.on('checkpassword', function (data) {
+			if (data == process.env.PASS) {
+				socket.emit('checkpassword', true)
+			} else {
+				socket.emit('checkpassword', false)
 			}
 		})
 
-
+		socket.on('end', function (data) {
+			io.sockets.emit('end', JSON.parse(fs.readFileSync('server/elecciones.json')))
+			setTimeout(() => process.exit(0), 1000)
+		})
 
 	})
 })
 
-
-//io.on('connection', function (socket) {
-	// board.on('ready', function() {
-	// 	socket.emit('boad-ready', {board: board.type , port: board.port} )
-	// })
-	// console.log(`alguien se ha conectado con sockets`)
-	// socket.emit('messages', messages)
-
-	// socket.on('new-message', function (data) {
-	// 	messages.push(data)
-	// 	io.sockets.emit('messages', messages)
-	// })
-//})
 
 var port = process.env.PORT || 8080
 
